0e9a2c49a4a792bdec654d04a5b7a988709d3ea2
Baihaki <baihakidemiandraputra@gmail.com>
2022-11-19 12:24:24 +0700

    quiz

---
+++ quiz.js
@@ -0,0 +1,37 @@
+const dataAneh = {
+  statusCode: 200,
+  listing: [
+    {
+      id: 1,
+      name: "budi",
+      hobbies: [
+        {
+          day: "monday",
+          activity: "running",
+          location: "sport hall",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "ahmad",
+      hobbies: [
+        {
+          day: "monday",
+          activity: "swimming",
+          location: "empang",
+        },
+      ],
+    },
+  ],
+};
+
+// const isiAhmad = dataAneh.listing[1].hobbies;
+// console.log(isiAhmad);
+
+const isiAhmad = dataAneh.listing.filter((item) => {
+  return item.name === "ahmad";
+});
+for (const i in isiAhmad[0].hobbies[0]) {
+  console.log(isiAhmad[0].hobbies[0][i]);
+}
